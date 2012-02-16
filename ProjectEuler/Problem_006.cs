a1b733a24799ee268e21ba29343031f6e9fd4085
James Wampler <AMER/jwampler@gettyimages.com>
2012-02-16 12:20:46 -0800

    Problem 006 Solution and Tests

---
+++ ProjectEuler/Problem_006.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+	public class Problem_006
+	{
+
+		public long CalculateDifferenceBetweenSumOfSquaresAndSquareOfSums(int topOfRange)
+		{
+
+			long sumOfSquares = 0;
+			long squareOfSums = 0;
+
+			for (int i = 1; i <= topOfRange; i++)
+			{
+
+				sumOfSquares += (long)Math.Pow(i, 2);
+				squareOfSums += i;
+
+			}
+
+			squareOfSums = (long)Math.Pow(squareOfSums, 2);
+
+			return squareOfSums - sumOfSquares;
+		}
+
+
+	}
+}
---
+++ tests/ProjectEuler.Tests/Problem_006Test.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+	[TestFixture]
+	public class Problem_006Test
+	{
+
+		[Test]
+		public void Calculate_Difference_Between_Sum_Of_Squares_And_Sqaure_of_Sums_For_10_Equals_2640()
+		{
+
+			var problem = new Problem_006();
+			var result = problem.CalculateDifferenceBetweenSumOfSquaresAndSquareOfSums(10);
+
+			Assert.That(result, Is.EqualTo(2640));
+
+		}
+
+		[Test]
+		public void Calculate_Difference_Between_Sum_Of_Squares_And_Sqaure_of_Sums_For_100_Equals_25164150()
+		{
+
+			var problem = new Problem_006();
+			var result = problem.CalculateDifferenceBetweenSumOfSquaresAndSquareOfSums(100);
+
+			Assert.That(result, Is.EqualTo(25164150));
+
+		}
+	}
+}
