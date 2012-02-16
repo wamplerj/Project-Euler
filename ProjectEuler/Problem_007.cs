25ecb42804330d7cc71d57586b367108bda24a33
James Wampler <AMER/jwampler@gettyimages.com>
2012-02-16 12:38:52 -0800

    Problem 007 Solution and Tests

---
+++ ProjectEuler/Problem_007.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using ProjectEuler.Helpers;
+
+
+namespace ProjectEuler
+{
+	public class Problem_007
+	{
+		public long FindNthPrime(int n)
+		{
+			int index = 1;
+			int primesFound = 0;
+			long nthPrime = 0;
+
+			while (primesFound < n)
+			{
+				if (MathHelper.IsPrime(index))
+					primesFound++;
+
+				if (primesFound == n)
+					nthPrime = index;
+
+				index++;
+
+			}
+
+			return nthPrime;
+		}
+	}
+}
---
+++ tests/ProjectEuler.Tests/Problem_007Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+	[TestFixture]
+	public class Problem_007Test
+	{
+		[Test]
+		public void The_5th_Prime_Number_Is_11()
+		{
+
+			var problem = new Problem_007();
+			var result = problem.FindNthPrime(5);
+
+			Assert.That(result, Is.EqualTo(11));
+
+		}
+
+
+		[Test]
+		public void The_10001st_Prime_Is_104743()
+		{
+			var problem = new Problem_007();
+			var result = problem.FindNthPrime(10001);
+
+			Assert.That(result, Is.EqualTo(104743));
+		}
+	}
+}
