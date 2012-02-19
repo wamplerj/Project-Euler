57abd2dcf3952031ada5f047cc18fb41f0f49d11
James Wampler <james@jameswampler.com>
2012-02-18 21:15:55 -0800

    Problem 014 solution and test

---
+++ ProjectEuler/Problem_014.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+	public class Problem_014
+	{
+
+		private Dictionary<long, int> chains = new Dictionary<long, int>();
+
+		public long GetLargestChain(long start)
+		{
+			long largestNumber = 0;
+			int largestCount = 0;
+			for (long i = 2; i < start; i++)
+			{
+				int count = BuildChain(i);
+
+				if(!chains.ContainsKey(i))
+					chains.Add(i, count);
+
+				if (count > largestCount)
+				{
+					largestCount = count;
+					largestNumber = i;
+				}
+			}
+
+			return largestNumber;
+		}
+
+		private int BuildChain(long start)
+		{
+
+			int count = 0;
+			while (start != 1)
+			{
+				if (start % 2 == 0)
+					start /= 2;
+				else
+					start = (3 * start) + 1;
+
+				if (chains.ContainsKey(start))
+				{
+					count = count + chains[start];
+					break;
+				}
+
+				count++;
+			}
+
+			return count;
+		}
+	}
+}
---
+++ tests/ProjectEuler.Tests/Problem_014Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+	[TestFixture]
+	public class Problem_014Test
+	{
+
+		[Test]
+		public void Find_Number_With_The_Largest_Chain_Of_Terms()
+		{
+
+			var problem = new Problem_014();
+			var result = problem.GetLargestChain(1000000);
+
+			Assert.That(result, Is.EqualTo(837799));
+
+		}
+	}
+}
