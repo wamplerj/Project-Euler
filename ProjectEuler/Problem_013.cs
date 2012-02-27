d324bb06d7c0cec9727fbecba71ddaa172785544
James Wampler <AMER/jwampler@gettyimages.com>
2012-02-27 15:31:40 -0800

    Problem 13 solution and tests

---
+++ ProjectEuler/Problem_013.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using ProjectEuler.Helpers;
+
+namespace ProjectEuler
+{
+    public class Problem_013
+    {
+        public string GetFirstXDigitsFromSumOfArrayOfNumbers(BigInteger[] numbers)
+        {
+            BigInteger sum = BigInteger.Zero;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            string sumString = sum.ToString();
+
+            return sumString.Substring(0, 10);
+        }
+    }
+}
