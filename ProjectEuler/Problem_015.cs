02a183b199d7d0833a568f39eecf486c0b34c193
James Wampler <AMER/jwampler@gettyimages.com>
2012-02-28 09:59:30 -0800

    Problem 15 solution and code

---
+++ ProjectEuler/Problem_015.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem_015
+    {
+        public long CalculateGridRoutes(int rows, int columns)
+        {
+            return BinomialCoefficient(rows*2, rows);
+        }
+
+        public static long BinomialCoefficient(int n, int k)
+        {
+
+            if (k > (n - k))
+                k = n - k;
+
+            long c = 1;
+
+            for (int i = 0; i < k; i++)
+            {
+                c = c * (n - i);
+                c = c / (i + 1);
+            }
+
+            return c;
+        }
+    }
+}
---
+++ tests/ProjectEuler.Tests/Problem_015Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem_015Tests
+    {
+
+        [Test]
+        public void When_Calculating_Routes_In_A_2_By_2_Grid_There_Are_6()
+        {
+            var problem = new Problem_015();
+            var result = problem.CalculateGridRoutes(2, 2);
+
+
+            Assert.That(result, Is.EqualTo(6));
+
+
+        }
+
+        [Test]
+        public void When_Calculating_Routes_In_A_20_By_20_Grid_There_Are_137846528820()
+        {
+            var problem = new Problem_015();
+            var result = problem.CalculateGridRoutes(20, 20);
+
+
+            Assert.That(result, Is.EqualTo(137846528820));
+
+
+        }
+
+    }
+}
