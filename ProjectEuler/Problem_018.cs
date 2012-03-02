fc86bbfc8670f79e7c18597080e8fce288b5e07e
James Wampler <AMER/jwampler@gettyimages.com>
2012-03-02 11:50:31 -0800

    Problem 18 solution and tests

---
+++ ProjectEuler/Problem_018.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem_018
+    {
+
+        private List<List<int>> _triangle = new List<List<int>>();
+        private Dictionary<string, int> _calculatedValues = new Dictionary<string, int>();
+
+        public void LoadTriangle(string fileName)
+        {
+            using(var reader = new StreamReader(Environment.CurrentDirectory + "\\Data\\" + fileName))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(new[] {' '}).Select(i => int.Parse(i)).ToList();
+
+                    _triangle.Add(values);
+                }
+            }
+
+        }
+
+        private void SetLargestSum(int i, int j, int currentSum)
+        {
+
+            string leftKey = CreateKey(i + 1, j);
+            string rightKey = CreateKey(i + 1, j + 1);
+
+            int leftSum = currentSum + _triangle[i + 1][j];
+            if(!_calculatedValues.ContainsKey(leftKey))
+            {
+                _calculatedValues.Add(leftKey, leftSum);
+            }
+            else
+            {
+                int currentLeftSum = _calculatedValues[leftKey];
+                if (leftSum > currentLeftSum)
+                    _calculatedValues[leftKey] = leftSum;
+            }
+
+            int rightSum = currentSum + _triangle[i + 1][j + 1];
+            _calculatedValues.Add(rightKey, rightSum);
+
+        }
+
+
+
+        public int FindLargestSum()
+        {
+            int largestSum = 0;
+
+            for (int i = 0; i < _triangle.Count - 1; i++ )
+            {
+                for(int j = 0; j < _triangle[i].Count; j++ )
+                {
+                    string currentKey = CreateKey(i, j);
+
+                    var currentSum = _calculatedValues.ContainsKey(currentKey)
+                                         ? _calculatedValues[currentKey]
+                                         : _triangle[i][j];
+
+                    SetLargestSum(i, j, currentSum);
+
+                }
+            }
+
+            int lastRow = _triangle.Count - 1;
+            for(int j = 0; j < _triangle[lastRow].Count; j++)
+            {
+                string key = CreateKey(lastRow, j);
+
+                int sum = _calculatedValues[key];
+                if (sum > largestSum)
+                    largestSum = sum;
+            }
+
+            return largestSum;
+        }
+
+        private string CreateKey(int i, int j)
+        {
+            return string.Format("{0}x{1}", i, j);
+        }
+    }
+}
---
+++ tests/ProjectEuler.Tests/Problem_018Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem_018Test
+    {
+
+
+        [Test]
+        public void Verify_That_The_Largest_Sum_Of_The_Sample_Triangle_Is_23()
+        {
+
+            var problem = new Problem_018();
+            problem.LoadTriangle("018-sample.txt");
+            var result = problem.FindLargestSum();
+
+            Assert.That(result, Is.EqualTo(23));
+
+        }
+
+
+        [Test]
+        public void Find_The_Largest_Sum_Navigating_A_Triangle()
+        {
+
+            var problem = new Problem_018();
+            problem.LoadTriangle("018.txt");
+            var result = problem.FindLargestSum();
+
+            Assert.That(result, Is.EqualTo(1074));
+        }
+
+
+    }
+}
