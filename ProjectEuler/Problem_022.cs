aff700ac0a66836e53802f9b2ff03bd3455b9897
James Wampler <james@jameswampler.com>
2012-03-02 14:33:41 -0800

    Problem 22 solution and tests

---
+++ ProjectEuler/Problem_022.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class Problem_022
+    {
+        private readonly int characterOffset = 64;
+
+        public long CalculateAllNameScores()
+        {
+            long sum = 0;
+
+            var names = GetNamesFromFile();
+
+            for(int i = 0; i < names.Count; i++)
+            {
+                var nameScore = GetNameScore(names[i]);
+                nameScore = nameScore*(i + 1);
+
+                sum += nameScore;
+            }
+
+            return sum;
+        }
+
+        public int GetNameScore(string name)
+        {
+            int sum = 0;
+
+            foreach(var letter in name.Trim().ToUpper())
+            {
+                int value = (int) letter - characterOffset;
+
+                Debug.Assert(value >= 1 || value <= 26, "Letter is " + letter);
+
+                sum += value;
+            }
+
+            return sum;
+        }
+
+        private List<string> GetNamesFromFile()
+        {
+            StringBuilder temp = new StringBuilder();
+            var names = new List<string>();
+
+            using (var reader = new StreamReader(Environment.CurrentDirectory + "\\Data\\022.txt"))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    temp.AppendLine(reader.ReadLine());
+                }
+            }
+
+            temp = temp.Replace("\"", "");
+            names = temp.ToString().Split(new[] {','}).ToList();
+            names.Sort();
+
+            return names;
+        }
+    }
+}
---
+++ tests/ProjectEuler.Tests/Problem_022Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    class Problem_022Test
+    {
+        [Test]
+        public void Calculate_The_Sum_Of_All_Names_In_List()
+        {
+            var problem = new Problem_022();
+            var result = problem.CalculateAllNameScores();
+
+            Assert.That(result, Is.EqualTo(871198282));
+
+        }
+
+        [Test]
+        public void Verify_NameScore_Of_COLIN_Is_53()
+        {
+            var problem = new Problem_022();
+            var result = problem.GetNameScore("COLIN");
+
+            Assert.That(result, Is.EqualTo(53));
+
+        }
+
+    }
+}
