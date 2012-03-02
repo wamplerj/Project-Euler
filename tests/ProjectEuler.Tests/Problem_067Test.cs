26993f89de778ba9346a89933f277c0245ecbe60
James Wampler <AMER/jwampler@gettyimages.com>
2012-03-02 11:58:13 -0800

    Problem 067  tests using Problem 18 solution

---
+++ tests/ProjectEuler.Tests/Problem_067Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem_067Test
+    {
+
+        [Test]
+        public void Find_Largest_Sum_From_Triangle_With_100_Rows()
+        {
+            var problem = new Problem_018();
+            problem.LoadTriangle("067.txt");
+            var result = problem.FindLargestSum();
+
+            Assert.That(result, Is.EqualTo(7273));
+
+        }
+
+    }
+}
