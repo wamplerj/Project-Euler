75324add60083e1390eaa8872847e766952c04db
James Wampler <james@jameswampler.com>
2012-03-02 15:05:45 -0800

    Problem 25 solution and tests

---
+++ ProjectEuler/Problem_025.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class Problem_025
+    {
+        public int GetFirstFibonacciTermWithOneThousandDigits()
+        {
+            BigInteger term1 = 1;
+            BigInteger term2 = 2;
+            BigInteger currentTerm = term1 + term2;
+            int termCount = 3;
+
+			while (currentTerm.ToString().Length < 1000)
+			{
+			    termCount++;
+				term1 = term2;
+				term2 = currentTerm;
+				currentTerm = term1 + term2;
+
+			}
+
+			return termCount + 1;
+		}
+    }
+}
---
+++ tests/ProjectEuler.Tests/Problem_025Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    class Problem_025Test
+    {
+
+        [Test]
+        public void Verify_That_The_First_Fibonacci_Term_With_One_Thousand_Digits_Is_4782()
+        {
+            var problem = new Problem_025();
+            var result = problem.GetFirstFibonacciTermWithOneThousandDigits();
+
+            Assert.That(result, Is.EqualTo(4782));
+        }
+    }
+}
