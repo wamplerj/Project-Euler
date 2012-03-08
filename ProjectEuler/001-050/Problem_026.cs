a8969608a0036f02b02b8e3f9cb639f1632b9dcf
James Wampler <james@jameswampler.com>
2012-03-08 13:21:44 -0800

    Problem 26 solution and test

---
+++ ProjectEuler/001-050/Problem_026.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem_026
+    {
+        public int CalculateLargestReoccuringCycle(int n)
+        {
+
+            int sequenceLength = 0;
+
+            for (int i = n; i > 1; i--)
+            {
+                if (sequenceLength >= i)
+                {
+                    break;
+                }
+
+                int[] foundRemainders = new int[i];
+                int value = 1;
+                int position = 0;
+
+                while (foundRemainders[value] == 0 && value != 0)
+                {
+                    foundRemainders[value] = position;
+                    value *= 10;
+                    value %= i;
+                    position++;
+                }
+
+                if (position - foundRemainders[value] > sequenceLength)
+                {
+                    sequenceLength = position - foundRemainders[value];
+                }
+
+            }
+
+            return sequenceLength;
+        }
+    }
+}
