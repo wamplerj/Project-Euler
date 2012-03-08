68f72fb2ce86b8c75bd84fd787ff372b1724f453
James Wampler <james@jameswampler.com>
2012-03-08 13:23:24 -0800

    Merge Problem-26 branch

---
+++ tests/ProjectEuler.Tests/001-050/Problem_026Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class Problem_026Test
+    {
+
+        [Test]
+        public void Calculate_The_Largest_Reoccuring_Cycle_Under_1000()
+        {
+            var problem = new Problem_026();
+            var result = problem.CalculateLargestReoccuringCycle(1000);
+
+            Assert.That(result, Is.EqualTo(982));
+        }
+
+
+    }
+}
